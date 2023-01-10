ba3ec32c17191b3044d52c54604ba7267a0f708d
sowoleuthman017 <112944718+sowoleuthman017@users.noreply.github.com>
2023-01-10 12:39:16 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function sendEmail() {
         Host : "smtp.elasticemail.com",
         Username : "ayokaz11@gmail.com",
         Password : "2D64806845CD49B2714BC2A6128A5A10276C",
-        To : 'ayokaz11@gmail.com',
+        To : 'shuttercrib@gmail.com',
         From : "ayokaz11@gmail.com",
         Subject : "BOOK SHUTTERCRIB",
         Body : "NAME: " + document.getElementById("name").value
@@ -13,7 +13,7 @@ function sendEmail() {
         +"<br> EVENT DATE: " + document.getElementById("date").value
         +"<br> EVENT DETAILS: " + document.getElementById("eventdetails").value
     }).then(
-      document.getElementById("hidden").innerHTML = "Thank You For Choosing ShutterCrib Photography!!!"
+        document.getElementById("hidden").innerHTML = "Thank You For Choosing ShutterCrib Photography!!!"
     );
 }
   
